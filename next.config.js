81089dffd8c15afa4daca9ebed286c2facdcb74e
Shinobu Hayashi <42742053+PhilisPaxil@users.noreply.github.com>
2019-06-04 17:14:07 +0900

    [fix]webpack

--- next.config.js
+++ next.config.js
@@ -14,12 +14,47 @@ module.exports = withPlugins([
                         loader: 'url-loader'
                     }
                 })
+
+                config.module.rules.push({
+                    test: /\.scss$/,
+                    use: [
+                        'style-loader',
+                        'css-loader',
+                        'postcss-loader',
+                        'sass-loader',
+                        {
+                            loader: 'sass-resources-loader',
+                            options: {
+                                // Provide path to the file with resources
+                                resources: './assets/css/resources.scss',
+
+                                // Or array of paths
+                                resources: ['./path/to/vars.scss', './path/to/mixins.scss']
+                            },
+                        },
+                    ],
+                })
+
                 return config
             }
         }
     ],
     [
         withTypescript,
-        {}
+        {
+            webpack: function (config) {
+                config.module.rules.push({
+                    test: /\.ts(x?)$/,
+                    use: {
+                        loader: 'ts-loader',
+                        options: {
+                            transpileOnly: true
+                        }
+                    }
+
+                })
+                return config
+            }
+        }
     ]
 ])
