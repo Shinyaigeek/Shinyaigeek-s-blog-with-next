5f360acd197a5c3a3ffec8c2f1b637d147131877
Shinobu Hayashi <42742053+PhilisPaxil@users.noreply.github.com>
2019-07-25 07:09:52 +0900

    [add]contact

--- components/Header.tsx
+++ components/Header.tsx
@@ -8,19 +8,36 @@ export default function Header() {
 	return (
 		<div className="header">
 			<Link prefetch href="/">
-				<a href="/" className="toHome">しにゃいの学習帳</a>
+				<a href="/" className="toHome">
+					しにゃいの学習帳
+				</a>
 			</Link>
-            <div className="contents">
-                <Link prefetch href="/">
-                    <a href="/" className="toHome">ブログ</a>
-                </Link>
-                <Link prefetch href="/">
-                    <a href="/" className="toHome">プロフィール</a>
-                </Link>
-                <Link prefetch href="/">
-                    <a href="/" className="toHome">作品集</a>
-                </Link>
-            </div>
+			<div className="contents">
+				<div className="to">
+					<Link prefetch href="/">
+						<a href="/" className="toHome">
+							ブログ
+						</a>
+					</Link>
+				</div>
+				<div className="to">
+					<Link prefetch href="/">
+						<a href="/" className="toHome">
+							プロフィール
+						</a>
+					</Link>
+				</div>
+				<div className="to">
+					<Link prefetch href="/">
+						<a href="/" className="toHome">
+							作品集
+						</a>
+					</Link>
+				</div>
+                <div className="to">
+                    コンタクト
+                </div>
+			</div>
 		</div>
 	);
 }
