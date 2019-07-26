34a190aa47e6cb83e49b511333ddce53f5bce240
Shinobu Hayashi <42742053+PhilisPaxil@users.noreply.github.com>
2019-07-26 14:18:13 +0900

    [fix]

--- components/Header.tsx
+++ components/Header.tsx
@@ -60,16 +60,32 @@ export default function Header(props: any) {
 					onClose={() => props.handleMenuFlag(false)}
 					width="100%"
 					closable={false}
+					className="drawer"
 				>
-					<ul className="hamberger--menus">
-						<li>REM</li>
-						<li>SH</li>
-						<li>TEAM</li>
-						<li>COMPANY</li>
-						<li onClick={() => props.handleContactFlag(true)}>
-							CONTACT
-						</li>
-					</ul>
+					<div className="menu--list">
+						<div className="to">
+							<Link prefetch href="/">
+								<a href="/" className="toHome">
+									ブログ
+								</a>
+							</Link>
+						</div>
+						<div className="to">
+							<Link prefetch href="/">
+								<a href="/" className="toHome">
+									プロフィール
+								</a>
+							</Link>
+						</div>
+						<div className="to">
+							<Link prefetch href="/">
+								<a href="/" className="toHome">
+									作品集
+								</a>
+							</Link>
+						</div>
+						<div className="to">コンタクト</div>
+					</div>
 				</Drawer>
 			</div>
 		</div>
--- components/Welcome1.tsx
+++ components/Welcome1.tsx
@@ -9,7 +9,6 @@ import "../assets/css/weocome1.scss";
 export default function Welcome1() {
 	return (
 		<div className="welcome1">
-			<HeyWorld />
 			<ScrollDown />
 			<Animation />
 		</div>
