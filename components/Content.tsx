0b46cbfb4d1a1c16ab72ad30faed78a9cfcddde2
Shinobu Hayashi <42742053+PhilisPaxil@users.noreply.github.com>
2019-08-02 12:13:11 +0900

    [fix]

--- components/Content.tsx
+++ components/Content.tsx
@@ -1,8 +1,10 @@
 import React, { useEffect } from "react";
 
 import ReactMarkdown from "react-markdown";
+import ReactHtmlParser from 'react-html-parser';
 
 import CodeBlock from "./CodeBlock";
+import Heading from "./Heading"
 
 import "../assets/css/Content.scss";
 
@@ -10,34 +12,30 @@ export default function Content(content: any) {
 	useEffect(() => {
 		const heading3 = document.querySelectorAll("h3");
 		const content: any = document.querySelector(".blog__content");
-		if (heading3.length) {
-			const headings = document.createElement("div");
-			headings.setAttribute("class", "headings");
-			let a;
-			heading3.forEach((tag: any, index: number) => {
-				tag.setAttribute("id", index);
-				a = document.createElement("a");
-				a.textContent = tag.innerText;
-				a.setAttribute("href", "#" + index);
-				headings.appendChild(a);
-			});
-			content.insertBefore(headings, content.firstChild);
-		}
+		// if (heading3.length) {
+		// 	const headings = document.createElement("div");
+		// 	headings.setAttribute("class", "headings");
+		// 	let a;
+		// 	heading3.forEach((tag: any, index: number) => {
+		// 		tag.setAttribute("id", index);
+		// 		a = document.createElement("a");
+		// 		a.textContent = tag.innerText;
+		// 		a.setAttribute("href", "#" + index);
+		// 		headings.appendChild(a);
+		// 	});
+		// 	content.insertBefore(headings, content.firstChild);
+		// }
     });
 
     console.log(content)
 
 	return (
 		<div className="blog__content">
-			<ReactMarkdown
-				source={content.content}
-				escapeHtml={false}
-				className="blog"
-				renderers={{
-                    code: CodeBlock,
-                    h3:"h4"
-				}}
-			/>
+			<Heading content={content.content}/>
+
+			<div className="blog">
+				{ReactHtmlParser(content.content)}
+			</div>
 		</div>
 	);
 }
