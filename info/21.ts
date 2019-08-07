b0b1d58c9758dd16179713af7740b4e3a2c8ebff
Shinobu Hayashi <42742053+PhilisPaxil@users.noreply.github.com>
2019-08-07 21:34:28 +0900

    [remove]post

--- info/21.ts
+++
@@ -1,9 +0,0 @@
-
-module.exports = {
-    name:"アニメーション実装したけりゃcanvasrタグから逃げるな",
-    path:"/21",
-    tag:["Programing"],
-    description:`canvasタグを勉強した時のメモ`,
-    img:"../static/icon.jpg",
-    date:"2019/5/22"
-}
--- info/22.ts
+++
@@ -1,8 +0,0 @@
-module.exports = {
-	name: "進捗",
-	path: "/2",
-	tag: ["Programing", "Blog"],
-	description: `進捗報告`,
-	img: "../static/icon.jpg",
-	date: "2018/10/21"
-};
